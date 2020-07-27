8b5b959492ec618fc6927111567c131be4e31a2b
Byron Barkhuizen <42861968+byronbark@users.noreply.github.com>
2020-07-27 09:57:31 +0200

    Update 1_Pcap2Session.ps1

--- PCAP Manipulation/1_Pcap2Session.ps1
+++ PCAP Manipulation/1_Pcap2Session.ps1
@@ -1,10 +1,10 @@
 foreach($f in gci 1_Pcap *.pcap)
 {
-    0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -o 2_Session\AllLayers\$($f.BaseName)-ALL
+    splitcap -p 50000 -b 50000 -r $f.FullName -o 2_Session\AllLayers\$($f.BaseName)-ALL
     # 0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -s flow -o 2_Session\AllLayers\$($f.BaseName)-ALL
     gci 2_Session\AllLayers\$($f.BaseName)-ALL | ?{$_.Length -eq 0} | del
 
-    0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -o 2_Session\L7\$($f.BaseName)-L7 -y L7
+    splitcap -p 50000 -b 50000 -r $f.FullName -o 2_Session\L7\$($f.BaseName)-L7 -y L7
     # 0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -s flow -o 2_Session\L7\$($f.BaseName)-L7 -y L7
     gci 2_Session\L7\$($f.BaseName)-L7 | ?{$_.Length -eq 0} | del
 }
