801e45bb1238713632b6681c957d31caf6dbc175
Byron Barkhuizen <42861968+byronbark@users.noreply.github.com>
2020-08-05 07:22:09 +0200

    Update 1B_MAC2Session.ps1
    Added layer option correction

--- PCAP Manipulation/1B_MAC2Session.ps1
+++ PCAP Manipulation/1B_MAC2Session.ps1
@@ -4,7 +4,7 @@ foreach($f in gci .\2A_MAC\AllLayers *.pcap)
     # 0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -s flow -o 2A_MAC\AllLayers\$($f.BaseName)-ALL
     gci 2B_Session\AllLayers\$($f.BaseName)-ALL | ?{$_.Length -eq 0} | del
 
-    splitcap -p 10000 -b 10000 -r $f.FullName -o 2B_Session\L7\$($f.BaseName)-L7
+    splitcap -p 10000 -b 10000 -r $f.FullName -o 2B_Session\L7\$($f.BaseName)-L7 -y L7
     # 0_Tool\SplitCap_2-1\SplitCap -p 50000 -b 50000 -r $f.FullName -s flow -o 2A_MAC\AllLayers\$($f.BaseName)-ALL
     gci 2B_Session\L7\$($f.BaseName)-L7 | ?{$_.Length -eq 0} | del
 }
