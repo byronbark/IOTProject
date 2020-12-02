ee9724d4c713aa0b66f031304b7be641541da0e0
Byron Barkhuizen <42861968+byronbark@users.noreply.github.com>
2020-12-02 13:03:12 +0100

    Add files via upload

--- TCA/tca.py
+++ TCA/tca.py
@@ -1,65 +1,100 @@
+import os
 import numpy as np
-import pandas as pd
-import json
-import da_tool.tca
-from tl_algs import tl_alg
-from sklearn.cluster import KMeans
-from sklearn.metrics.pairwise import euclidean_distances
+import scipy.io
+import scipy.linalg
+import sklearn.metrics
+from sklearn.neighbors import KNeighborsClassifier
 
-class TCA(tl_alg.Base_Transfer):
-        def __init__(self, test_set_X, test_set_domain, train_pool_X, train_pool_y,
-                 train_pool_domain, Base_Classifier, classifier_params={},
-                 rand_seed=None, dims=5, kernel_type='linear', kernel_param=1,
-                mu=1):
+print (os.getcwd())
 
-        super(TCA, self).__init__(
-            test_set_X,
-            test_set_domain,
-            train_pool_X,
-            train_pool_y,
-            train_pool_domain,
-            Base_Classifier,
-            rand_seed=rand_seed,
-            classifier_params=classifier_params,
-        )
+def kernel(ker, X1, X2, gamma):
+    ''' Decide metrics by type of kernel '''
+    K = None
+    if not ker or ker == 'primal':
+        K = X1
+    elif ker == 'linear':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T, np.asarray(X2).T)
+        else:
+            K = sklearn.metrics.pairwise.linear_kernel(np.asarray(X1).T)
+    elif ker == 'rbf':
+        if X2 is not None:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, np.asarray(X2).T, gamma)
+        else:
+            K = sklearn.metrics.pairwise.rbf_kernel(np.asarray(X1).T, None, gamma)
+    return K
 
-        self.dims = dims
+
+class TCA:
+    def __init__(self, kernel_type='primal', dim=30, lamb=1, gamma=1):
+        '''
+        Initialize the model parameters by the user
+        :param kernel_type: kernel, values: 'primal' or 'linear' or 'rbf'
+        :param dim: dimension after transfer
+        :param lamb: lambda value in equation
+        :param gamma: kernel bandwidth for rbf kernel
+        '''
         self.kernel_type = kernel_type
-        self.kernel_param = kernel_param
-        self.mu = mu
+        self.dim = dim
+        self.lamb = lamb
+        self.gamma = gamma
 
-    def create_classifier(self, X_train, y_train):
-        my_tca = da_tool.tca.TCA(dim=self.dims,kerneltype=self.kernel_type,
-                kernelparam=self.kernel_param, mu=self.mu)
-        train_TCA_X, target_test_TCA_X, __ = \
-            my_tca.fit_transform(X_train, self.test_set_X)
+    def fit(self, Xs, Xt):
+        '''
+        Transform Xs and Xt
+        :param Xs: ns * n_feature, source feature
+        :param Xt: nt * n_feature, target feature
+        :return: Xs_new and Xt_new after TCA transformation
+        Guided by TCA Paper algorithms for transfer component analysis
+        '''
+        X = np.hstack((Xs.T, Xt.T))
+        X /= np.linalg.norm(X, axis=0)
+        m, n = X.shape
+        ns, nt = len(Xs), len(Xt)
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        M = e * e.T
+        M = M / np.linalg.norm(M, 'fro')
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        K = kernel(self.kernel_type, X, None, gamma=self.gamma)
+        n_eye = m if self.kernel_type == 'primal' else n
+        a, b = np.linalg.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
+        w, V = scipy.linalg.eig(a, b)
+        ind = np.argsort(w)
+        A = V[:, ind[:self.dim]]
+        Z = np.dot(A.T, K)
+        Z /= np.linalg.norm(Z, axis=0)
+        Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
+        return Xs_new, Xt_new
 
-        # build and train classifier
-        clf = self.Base_Classifier(random_state=self.rand_seed,
-                **self.classifier_params)
-        clf.fit(train_TCA_X, y_train)
-        return clf.predict(target_test_TCA_X)
+    def fit_predict(self, Xs, Ys, Xt, Yt):
+        '''
+        Transform Xs and Xt, then make predictions on target using 1NN
+        :param Xs: ns * n_feature, source feature
+        :param Ys: ns * 1, source label
+        :param Xt: nt * n_feature, target feature
+        :param Yt: nt * 1, target label
+        :return: Accuracy and predicted_labels on the target domain
+        '''
+        Xs_new, Xt_new = self.fit(Xs, Xt)
+        clf = KNeighborsClassifier(n_neighbors=1)
+        clf.fit(Xs_new, Ys.ravel())
+        y_pred = clf.predict(Xt_new)
+        acc = sklearn.metrics.accuracy_score(Yt, y_pred)
+        return acc, y_pred
 
-    def train_filter_test(self):
-        pred_list = []
-        for proj in set(self.train_pool_domain):
-            X_train_subset = self.train_pool_X[
-                np.array(self.train_pool_domain) == proj]
-            y_train_subset = self.train_pool_y[X_train_subset.index]
-            predictions = self.create_classifier(X_train_subset, y_train_subset)
-            pred_list.append(predictions)
 
-        # average votes
-        votes = zip(*pred_list)
-        confidence_arr = np.mean(votes, 1)
-        predictions_arr = confidence_arr > 0.5
-        return (confidence_arr, np.array(predictions_arr))
-
-    def json_encode(self):
-        base = tl_alg.Base_Transfer.json_encode(self)
-        base.update({"dims" : self.dims,
-            "kernel_type" : self.kernel_type,
-            "kernel_param" : self.kernel_param,
-            "mu" : self.mu})
-
-        return base
+if __name__ == '__main__':
+    domains = ['alldata.mat.mat', 'allnewdata.mat.mat']
+    results=[]
+    for i in [2]:
+        for j in [3]:
+            if i != j:
+                src, tar = 'C:/Users/b-byr/Desktop/Stage/Collection/data/' + domains[i], 'C:/Users/b-byr/Desktop/Stage/Collection/data/' + domains[j]
+                src_domain, tar_domain = scipy.io.loadmat(src), scipy.io.loadmat(tar)
+                src_domain
+                Xs, Ys, Xt, Yt = src_domain['fts'], src_domain['labels'], tar_domain['fts'], tar_domain['labels']
+                tca = TCA(kernel_type='linear', dim=30, lamb=1, gamma=1)
+                acc, ypre = tca.fit_predict(Xs, Ys, Xt, Yt)
+                results.append(acc)
+                # It should print 0.910828025477707
+    print(results)
